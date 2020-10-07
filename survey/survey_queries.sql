4b546e1d5f6936c077bc20225de3c10601670d59
Gavin Fleming <gavin@kartoza.com>
2020-10-07 11:25:44 +0200

    tewaks to village cluster query

--- survey/survey_queries.sql
+++ survey/survey_queries.sql
@@ -1,8 +1,15 @@
 set search_path to wbr_survey,public;
 
---attempt at replacing village geometry with voronoi polygons  ABANDONED
+/*
+attempt at replacing village geometry with voronoi polygons  ABANDONED
 update survey_villages sv 
 set geom = st_multi(st_intersection(st_buffer(st_centroid(sv.geom)::geography,500)::geometry,vp.geom)) from voronoipolys vp where st_within(st_centroid(sv.geom),vp.geom);
+*/
+
+--replace villages from mergin db
+truncate survey_villages;
+insert into survey_villages (id,geom,name,scoping_report_id,sample)
+(select id,geom,name,scoping_report_id,sample from survey_villages_mergin);
 
 --once-off generate small buffers around villages (they were too big before)
 
@@ -14,7 +21,7 @@ with vp as
 )
 select (st_dump(geom)).path[1] as id, (st_dump(geom)).geom::geometry(Polygon,4326) as geom from vp;
 
---update polygons for those village that have points to encompass all the points in the village
+--update polygons for those villages that have points to encompass all the points in the village
 with 
 cv as 
 (select sv.id,name,count(*), st_multi(st_intersection(st_buffer(st_concavehull(st_collect(st_force2d(wbr.geometry)),0.5)::geography,100)::geometry,vp.geom)) as geom 
@@ -40,4 +47,4 @@ update survey_villages sv set geom = cv.geom from cv where cv.id = sv.id;
 update survey_villages sv set count = 0;
 with counts as (
 select sv.id,count(*) from sync_main.wbr wbr  join survey_villages sv  on st_within(wbr.geometry,sv.geom) group by sv.id)
-update survey_villages sv set count = counts.count from counts where sv.id = counts.id
+update survey_villages sv set count = counts.count from counts where sv.id = counts.id;
