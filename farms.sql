c8750258020fc81201b1dcde6664e55220d7d1dc
Gavin Fleming <gavin@kartoza.com>
2020-12-09 15:26:41 +0200

    various tweaks to reserve planning (farm selection) login including creating core_protected_areas layer for labelling the core zone and incorporating CBA protected areas

--- farms.sql
+++ farms.sql
@@ -42,7 +42,9 @@ update sapad_or_2020_q1 set geom = st_makevalid(geom) where not st_isvalid(geom)
 drop table protected;
 create table protected as
 with collection as
-(select geom from paes
+(select geom from "Waterberg_ProtectedAreas_2015_u35s"
+ union
+select geom from paes
 union
 select st_transform(geom,32735) geom from protected_areas
 union
@@ -50,12 +52,15 @@ select st_transform(geom,32735) geom from sapad_or_2020_q1)
 select row_number() over () id, ST_multi((ST_Dump(st_union(st_snaptogrid(c.geom,0.001)))).geom)::geometry(Multipolygon,32735) geom from collection c join aoi_wbr a on st_intersects(c.geom,a.geom)
 --row_number() doesn't work over SRF (Dump) so need another approach to get unique ids
 
+alter table protected drop column id;
+alter table protected add column id serial;
+
 ALTER TABLE public.protected
     ADD PRIMARY KEY (id);
 	
 CREATE INDEX idx_protected_gist_geom
     ON public.protected USING gist
-    (geom)	
+    (geom);	
 
 --prep waterberg BR zones
 create table biosphere_reserves_waterberg as
@@ -73,11 +78,12 @@ alter table farmportions add column zone character varying(20);
 
 --you can start here to re-run all queries below
 update farmportions set zone=null;
-update farmportions set zone = 'buffer' where _median > 73; --(also try mean, majority etc)
+update farmportions set zone = 'buffer' where __median > 70; --(also try mean, majority etc). _ for m6 stats. __ for m7 stats
 update farmportions fp set zone = 'core' 
  from protected p
  where zone = 'buffer' and --st_intersects(fp.geom,p.geom);
- st_within(st_centroid(fp.geom),p.geom);
+ --st_within(st_centroid(fp.geom),p.geom); --I think this is the reason some small PAES areas were missed - no farm centroids fell within them
+st_contains(fp.geom,st_pointonsurface(p.geom));
 
 --now that some farms are core, that intersect protected polys, reverse select all the other farms that intesect those protected polys that might not have been buffer and make them core too
 with p as
@@ -139,9 +145,41 @@ update farmportions fp set zone = 'core' from p where st_within(st_centroid(fp.g
 update farmportions fp set zone=null from wbr_limit lim where not st_intersects(fp.geom,lim.geom);
 
 --union zones
---drop table wbr_boundary_milestone6;
-create table wbr_boundary_milestone6 as
-select row_number() over() as id, zone, st_union(geom)::geometry(Multipolygon,32735) from farmportions
+drop table wbr_boundary_milestone7;
+create table wbr_boundary_milestone7 as
+select row_number() over() as id, zone, st_union(geom)::geometry(Multipolygon,32735) geom from farmportions
 where zone is not null
 group by zone;
+
+ALTER TABLE public.wbr_boundary_milestone7
+    ADD PRIMARY KEY (id);
+	
+CREATE INDEX idx_wbr_boundary_milestone7_gist_geom
+    ON public.wbr_boundary_milestone7 USING gist
+    (geom);
+
+--report on protected areas selected as proposed buffer core zone
+drop table core_protected_areas;
+create table core_protected_areas as
+with collection as
+(select geom, initcap(name) as name, 'CBA' as source from "Waterberg_ProtectedAreas_2015_u35s"
+ union
+select st_transform(geom,32735) geom, focus_area as name, 'NPAES' as source from npaes_focus_areas_completetable
+union
+select st_transform(geom,32735) geom, cur_nme as name, 'SANBI "protected_areas" or DEFF "sapad_or_2020_q1"' as source from protected_areas
+union
+select st_transform(geom,32735) geom, cur_nme as name, 'SANBI "protected_areas" or DEFF "sapad_or_2020_q1"' as source from sapad_or_2020_q1)
+select row_number() over () id, c.geom, c.name, c.source from collection c join wbr_boundary_milestone7 a on st_within(st_pointonsurface(c.geom),a.geom)--removed distinct on (name)
+where a.zone = 'core';
+
+--select distinct name from core_protected_areas where source <> 'NPAES' order by name ; --add source to distinct and order by
+
+ALTER TABLE public.core_protected_areas
+    ADD PRIMARY KEY (id);
+	
+CREATE INDEX idx_core_protected_areas_gist_geom
+    ON public.core_protected_areas USING gist
+    (geom);
+
+
 
